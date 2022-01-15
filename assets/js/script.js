420b0cb5b23af96d122d36854f1f988da455abb1
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2022-01-14 17:22:36 -0800

    submit button

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var searchFormEl = $('#search-form');
+
+function handleSearchFormSubmit(e) {
+  e.preventDefault();
+
+  var searchInputVal = $('#search-input').val();
+  var formatInputVal = $('#format-input').val();
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  var queryString = './search-result.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.on('submit', handleSearchFormSubmit); 
